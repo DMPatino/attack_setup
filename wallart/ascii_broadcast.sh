8a34b71dc5c8e37f8966e19d955428f4671cabad
DMPatino <167009116+DMPatino@users.noreply.github.com>
2025-10-10 09:10:12 -0400

    Create ascii_broadcast.sh

---
+++ wallart/ascii_broadcast.sh
@@ -0,0 +1,15 @@
+
+
+#!/bin/bash
+# Broadcast random ASCII art from /opt/ascii_art every 2 minutes
+
+ART_DIR="/opt/ascii_art"
+TAG="[Training Broadcast]"
+
+while true; do
+  FILE=$(find "$ART_DIR" -type f -name "*.txt" | shuf -n 1)
+  if [[ -f "$FILE" ]]; then
+    wall "$TAG $(cat "$FILE")"
+  fi
+  sleep 120
+done
